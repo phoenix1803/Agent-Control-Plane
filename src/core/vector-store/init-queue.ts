d38ee381bdc8910e9f6c581d8ed822c4d6710bfb
= <kanikayadav061@gmail.com>
2026-02-13 23:52:19 +0530

    feat: enhance TraceRecorder with optional indexing and improve queue initialization

--- src/core/vector-store/init-queue.ts
+++ src/core/vector-store/init-queue.ts
@@ -3,12 +3,18 @@ import 'dotenv/config'
 
 export class Init_Queue {
 
-    // public traceId :string;
-    // public agentId:string;
-    // public tracePath:string;
+    public traceId :string;
+    public agentId:string;
+    public tracePath:string;
 
-    constructor({traceId,agentId,tracePath}:{traceId:string,agentId:string,tracePath:string}) {
+    public constructor({traceId,agentId,tracePath}:{traceId:string,agentId:string,tracePath:string}) {
+        this.traceId = traceId
+        this.agentId = agentId
+        this.tracePath = tracePath
+        
+    }
 
+    async initQueue(){
         const queue = new Queue("upload_traces",{
             connection: {
                 host: "localhost",
@@ -16,10 +22,15 @@ export class Init_Queue {
                 password: process.env.REDIS_PASSWORD,
             }
         });
-        await queue.add("uplaod_traces_job", { 
-            traceId,agentId,tracePath
-        })
-}
+        
 
+        await queue.add("uplaod_traces_job", { 
+            traceId:this.traceId,
+            agentId:this.agentId,
+            tracePath:this.tracePath
+        })
+
+
+    }
 
 }
